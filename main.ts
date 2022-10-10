a481436924b4ff56a72f962026bca9e1cf8c8d9d
tmheducation <68049755+tmheducation@users.noreply.github.com>
2022-10-10 11:17:24 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,2 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    basic.setLedColor(0x00ffff)
-    basic.showString("LOS")
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        # . # . #
-        . # . # .
-        . . # . .
-        `)
-})
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    led.stopAnimation()
-})
+basic.setLedColor(0x00ffff)
+basic.showString("Hallo!!!")
